0c3888b2c04deffd39aba3f6c07696146054d738
GabListeM <gabrielncblender@gmail.com>
2023-04-19 11:13:54 -0300

    Add project files.

---
+++ WebApp_Manha/Models/ClienteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebApp_Manha.Models
+{
+
+	public class ClienteViewModel
+	{
+		public long Id { get; set; }
+		public string Nome { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
